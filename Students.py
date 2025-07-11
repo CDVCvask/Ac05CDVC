649c66abd478c1b7d3e54ac52d6b5b348e59dc68
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-07-11 10:07:48 -0600

    Creando la clase

--- Students.py
+++ Students.py
@@ -1,1 +1,8 @@
-print("Iniciando")
+class Student:
+    def __init__(self,name,id,Carrer,fnote):
+        self.name = name
+        self.ID = id
+        self.Carrer = Carrer
+        self.Fnote = fnote
+def MENU():
+    print("Menu de estudiantes")
